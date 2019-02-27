09336cba8c6ed0b5e9415477eca23b558927b7bd
Hailey Somerville <hailey@hailey.lol>
2019-02-28 01:26:52 +1100

    pull out hexmap and add print32

--- src/task.c
+++ src/task.c
@@ -14,16 +14,33 @@ print(const char* msg)
     }
 }
 
+static const char* hexmap = "0123456789abcdef";
+
 static void
 print16(uint16_t val)
 {
-    const char hexmap[] = "0123456789abcdef";
     char buf[5];
     buf[0] = hexmap[(val >> 12) & 0xf];
     buf[1] = hexmap[(val >>  8) & 0xf];
     buf[2] = hexmap[(val >>  4) & 0xf];
     buf[3] = hexmap[(val >>  0) & 0xf];
     buf[4] = 0;
+    print(buf);
+}
+
+static void
+print32(uint32_t val)
+{
+    char buf[9];
+    buf[0] = hexmap[(val >> 28) & 0xf];
+    buf[1] = hexmap[(val >> 24) & 0xf];
+    buf[2] = hexmap[(val >> 20) & 0xf];
+    buf[3] = hexmap[(val >> 16) & 0xf];
+    buf[4] = hexmap[(val >> 12) & 0xf];
+    buf[5] = hexmap[(val >>  8) & 0xf];
+    buf[6] = hexmap[(val >>  4) & 0xf];
+    buf[7] = hexmap[(val >>  0) & 0xf];
+    buf[8] = 0;
     print(buf);
 }
 
@@ -376,7 +393,6 @@ interrupt(regs_t* regs)
         panic("Invalid opcode");
     }
 
-    const char hexmap[] = "0123456789abcdef";
     char msg[] = "unhandled interrupt 00";
     msg[sizeof(msg) - 3] = hexmap[(regs->interrupt >> 4) & 0xf];
     msg[sizeof(msg) - 2] = hexmap[(regs->interrupt >> 0) & 0xf];
